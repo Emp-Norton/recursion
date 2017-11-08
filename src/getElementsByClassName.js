eb14cb50aa243affbddab759b74761d00aa185c0
Emp-Norton <jamesjelenko@gmail.com>
2017-11-07 16:58:24 -0800

    Passing tests, but need to rewrite using recursion.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,12 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var elements = document.all
+  var elementsOfClass = [];
+
+  for (var elementIndex = 0; elementIndex < elements.length; elementIndex++){
+  	if (elements[elementIndex].className.includes(className)) elementsOfClass.push(elements[elementIndex])
+  }
+  return elementsOfClass
 };
